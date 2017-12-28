747ecb54f7e1327ee6a40361bd81365de283bc47
Djigo <djigolab@gmail.com>
2017-12-28 17:48:45 +0000

    There you go!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,44 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public isCollapsed = true;
+  columnName : String
+  
   constructor(public navCtrl: NavController) {
+
+ 
+
+
+  }
+  Expand(columnName){
+
+    let col1 = document.getElementsByClassName('column-1') as HTMLCollectionOf<HTMLElement>;
+    let col2 = document.getElementsByClassName('column-2') as HTMLCollectionOf<HTMLElement>;
+    let col3 = document.getElementsByClassName('column-3') as HTMLCollectionOf<HTMLElement>;
+
+    switch (columnName){
+      case 'column-1':
+      col2[0].removeAttribute('col-9');
+      col3[0].removeAttribute('col-9');
+      col1[0].setAttribute('col-9','');  
+      break;
+  
+      case 'column-2':
+          
+        col1[0].removeAttribute('col-9');
+        col3[0].removeAttribute('col-9');
+        col2[0].setAttribute('col-9','');         
+      
+        break;
+        case 'column-3':
+
+        col1[0].removeAttribute('col-9');
+        col2[0].removeAttribute('col-9');
+        col3[0].setAttribute('col-9','');
+        break; 
+    }
+    }
 
   }
 
-}
+
